602415c67b51fe8f84047540efc8f4085eac9dd2
LongLe <xuanlongvts@gmail.com>
2022-01-03 16:52:22 +0700

    enumeration nft

--- o4_zero_to_hero/1_nft_v_sdk_4.0/contract_nft/src/enumeration.rs
+++ o4_zero_to_hero/1_nft_v_sdk_4.0/contract_nft/src/enumeration.rs
@@ -2,6 +2,30 @@ use crate::*;
 
 #[near_bindgen]
 impl Contract {
+	pub fn nft_tokens(&self, from_index: Option<U128>, limit: Option<u64>) -> Vec<JsonToken> {
+		let keys = self.token_metadata_by_id.keys_as_vector();
+
+		//where to start pagination - if we have a from_index, we'll use that - otherwise start from 0 index
+		let start = u128::from(from_index.unwrap_or(U128(0)));
+
+		keys.iter()
+			.skip(start as usize)
+			.take(limit.unwrap_or(0) as usize)
+			.map(|token_id| self.nft_token(token_id.clone()).unwrap())
+			.collect()
+	}
+
+	// get the total supply of NFTs for a given owner
+	pub fn nft_supply_for_owner(&self, account_id: AccountId) -> U128 {
+		let tokens_for_owner_set = self.tokens_per_owner.get(&account_id);
+
+		if let Some(tokens_for_owner_set) = tokens_for_owner_set {
+			U128(tokens_for_owner_set.len() as u128)
+		} else {
+			U128(0)
+		}
+	}
+
 	pub fn nft_tokens_for_owner(
 		&self,
 		account_id: AccountId,
